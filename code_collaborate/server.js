aad59d65814822a90775b1578d4a2cfb3892e992
karthik <karthiknadar1204@gmail.com>
2023-11-26 17:05:25 +0530

    revoke clone feature

--- code_collaborate/server.js
+++ code_collaborate/server.js
@@ -1,134 +1,74 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const { Server } = require('socket.io');
+const express=require('express');
+const app=express();
+const http=require('http');
+const {Server}=require('socket.io');
 const ACTIONS = require('./src/Actions');
-const path = require('path');
-const { exec } = require('child_process');
-const simpleGit = require('simple-git');
-const axios = require('axios');
+const path = require('path'); 
 
-const server = http.createServer(app);
-const io = new Server(server);
+const server=http.createServer(app);
+const io=new Server(server);
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use(express.static('build')); 
+app.use((req,res,next)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
 
-const userSocketMap = {};
+const userSocketMap={
 
-function getAllConnectedClients(roomId) {
-  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
-    return {
-      socketId,
-      username: userSocketMap[socketId],
-    };
-  });
 }
 
-io.on('connection', (socket) => {
-  console.log('socket connected', socket.id);
-
-  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
-    userSocketMap[socket.id] = username;
-    socket.join(roomId);
-    const clients = getAllConnectedClients(roomId);
-
-    clients.forEach(({ socketId }) => {
-      io.to(socketId).emit(ACTIONS.JOINED, {
-        clients,
-        username,
-        socketId: socket.id,
-      });
-    });
-  });
-
-  socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
-    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
-  });
-
-  socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
-    io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
-  });
-
-// ... (existing code)
-
-socket.on(ACTIONS.CLONE_REPOSITORY, async ({ repositoryURL, roomId }) => {
-    const directory = `./repositories/${roomId}`;
-
-    try {
-        // Ensure that the directory exists before attempting to clone
-        exec(`mkdir -p ${directory}`, async (mkdirErr) => {
-            if (mkdirErr) {
-                console.error('Error creating directory:', mkdirErr);
-                if (socket.connected) {
-                    socket.emit(ACTIONS.CLONE_REPOSITORY_STATUS, {
-                        success: false,
-                        message: 'Error creating directory.',
-                    });
-                }
-                return;
-            }
-
-            const git = simpleGit(directory);
-
-            try {
-                await git.clone(repositoryURL, directory);
-            } catch (cloneError) {
-                console.error('Error cloning repository:', cloneError.message);
-                if (socket.connected) {
-                    socket.emit(ACTIONS.CLONE_REPOSITORY_STATUS, {
-                        success: false,
-                        message: 'Error cloning repository.',
-                    });
-                }
-                return;
-            }
-
-            if (socket.connected) {
-                socket.emit(ACTIONS.CLONE_REPOSITORY_STATUS, {
-                    success: true,
-                    message: 'Repository cloned successfully.',
-                });
-            }
-        });
-    } catch (error) {
-        console.error('Error cloning repository:', error.message);
-        if (socket.connected) {
-            socket.emit(ACTIONS.CLONE_REPOSITORY_STATUS, {
-                success: false,
-                message: 'Error cloning repository.',
-            });
+function getAllConnectedClients(roomId){
+    return Array.from(io.sockets.adapter.rooms.get(roomId)|| []).map((socketId)=>{
+        return {
+            socketId,
+            username:userSocketMap[socketId]
         }
-    }
-});
-  
-  socket.on(ACTIONS.PUSH_CHANGES, ({ roomId }) => {
-    const directory = `./repositories/${roomId}`;
-    const git = simpleGit(directory);
+    })
+}
 
-    git.add('./*').commit('Pushed changes').push('origin', 'main', (pushErr) => {
-      if (pushErr) {
-        console.error('Error pushing changes:', pushErr);
-      } else {
-        console.log('Changes pushed successfully.');
-      }
+io.on('connection',(socket)=>{
+    console.log('socket connected',socket.id)
+
+    socket.on(ACTIONS.JOIN,({roomId,username})=>{
+        userSocketMap[socket.id]=username;
+        socket.join(roomId);
+        const clients=getAllConnectedClients(roomId)
+
+        clients.forEach(({socketId})=>{
+            io.to(socketId).emit(ACTIONS.JOINED,{
+                clients,
+                username,
+                socketId:socket.id
+            })
+        })
     });
-  });
 
-  socket.on('disconnecting', () => {
-    const rooms = [...socket.rooms];
-    rooms.forEach((roomId) => {
-      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
-        socketId: socket.id,
-        username: userSocketMap[socket.id],
-      });
-    });
-    delete userSocketMap[socket.id];
-    socket.leave();
-  });
-});
 
-const PORT = process.env.PORT || 5001;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    socket.on(ACTIONS.CODE_CHANGE,({roomId,code})=>{
+        socket.in(roomId).emit(ACTIONS.CODE_CHANGE,{code});
+    })
+
+    socket.on(ACTIONS.SYNC_CODE,({socketId,code})=>{
+        io.to(socketId).emit(ACTIONS.CODE_CHANGE,{code});
+    })
+
+
+
+    socket.on('disconnecting',()=>{
+        const rooms=[...socket.rooms]
+        rooms.forEach((roomId)=>{
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED,{
+                socketId:socket.id,
+                username:userSocketMap[socket.id],
+            } );
+        });
+        delete userSocketMap[socket.id];
+        socket.leave();
+    })
+})
+
+
+const PORT=process.env.PORT || 5001;
+server.listen(PORT,()=> console.log(`Listening on port ${PORT}`))
+
+
--- code_collaborate/src/component/Editor.jsx
+++ code_collaborate/src/component/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Codemirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/javascript/javascript.js';
@@ -7,51 +7,47 @@ import 'codemirror/addon/edit/closetag';
 import 'codemirror/addon/edit/closebrackets';
 import ACTIONS from '../Actions';
 
-const Editor = ({ socketRef, roomId, onCodeChange }) => {
-  const [repositoryURL, setRepositoryURL] = useState('');
-  const [cloneStatus, setCloneStatus] = useState('');
-  const [githubToken, setGithubToken] = useState('');
-
+const Editor = ({ socketRef, roomId,onCodeChange }) => {
   const editorRef = useRef(null);
-  const syncingCodeRef = useRef(false);
-
-  const handlePushChanges = () => {
-    socketRef.current.emit(ACTIONS.PUSH_CHANGES, { roomId });
-  };
+  const syncingCodeRef = useRef(false); 
 
   useEffect(() => {
-    editorRef.current = Codemirror.fromTextArea(document.getElementById('realtimeEditor'), {
-      mode: {
-        name: 'javascript',
-        json: true,
-      },
-      theme: 'dracula',
-      autoCloseTags: true,
-      autoCloseBrackets: true,
-      lineNumbers: true,
-    });
+    async function init() {
+      editorRef.current = Codemirror.fromTextArea(document.getElementById('realtimeEditor'), {
+        mode: {
+          name: 'javascript',
+          json: true,
+        },
+        theme: 'dracula',
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
 
-    editorRef.current.on('changes', (instance, changes) => {
-      const { origin } = changes;
-      const code = instance.getValue();
-      onCodeChange(code);
-
-      if (origin !== 'setValue' && !syncingCodeRef.current) {
-        socketRef.current.emit(ACTIONS.CODE_CHANGE, {
-          roomId,
-          code,
-        });
-      }
-    });
-  }, [roomId, onCodeChange, socketRef]);
+      editorRef.current.on('changes', (instance, changes) => {
+        const { origin } = changes;
+        const code = instance.getValue();
+        onCodeChange(code);
+        
+        // Check if syncingCodeRef is set to true to prevent feedback loop
+        if (origin !== 'setValue' && !syncingCodeRef.current) {
+          socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+            roomId,
+            code
+          });
+        }
+      });
+    }
+    init();
+  }, []);
 
   useEffect(() => {
     if (socketRef.current) {
       socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
         if (code !== null) {
-          syncingCodeRef.current = true;
+          syncingCodeRef.current = true; // Set syncingCodeRef to true during syncing
           editorRef.current.setValue(code);
-          syncingCodeRef.current = false;
+          syncingCodeRef.current = false; // Set syncingCodeRef back to false after syncing
         }
       });
     }
@@ -61,58 +57,10 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
         socketRef.current.off(ACTIONS.CODE_CHANGE);
       }
     };
-  }, [socketRef]);
-
-  useEffect(() => {
-    const socket = socketRef.current;
-
-    if (socket) {
-      socket.on(ACTIONS.CLONE_REPOSITORY_STATUS, ({ success, message }) => {
-        if (success) {
-          setCloneStatus(message);
-        } else {
-          console.error('Error cloning repository:', message);
-          setCloneStatus('Error cloning repository.');
-        }
-      });
-    }
-
-    return () => {
-      if (socketRef.current) {
-        socketRef.current.off(ACTIONS.CLONE_REPOSITORY_STATUS);
-      }
-    };
-  }, [socketRef]);
-
-const handleCloneRepository = async () => {
-  try {
-    const cloneRequest = {
-      repositoryURL,
-      roomId,
-    };
-    socketRef.current.emit(ACTIONS.CLONE_REPOSITORY, cloneRequest);
-  } catch (error) {
-    console.error('Error cloning repository:', error.message);
-    setCloneStatus('Error cloning repository.');
-  }
-};
+  }, [socketRef.current]);
 
   return (
     <>
-      <input
-        type="text"
-        value={repositoryURL}
-        onChange={(e) => setRepositoryURL(e.target.value)}
-        placeholder="GitHub Repository URL"
-      />
-      <input
-        type="text"
-        value={githubToken}
-        onChange={(e) => setGithubToken(e.target.value)}
-        placeholder="GitHub Token"
-      />
-      <button onClick={handleCloneRepository}>Clone Repository</button>
-      <button onClick={handlePushChanges}>Push Changes</button>
       <textarea id="realtimeEditor"></textarea>
     </>
   );
